9a5d3ccbf3f2d6a12d0f5f72ca8770edda153484
Ayush Sharma <code.ayush@gmail.com>
2024-06-23 00:32:11 +0530

    Update C++ version to C++20.

--- src/components/C++/cpp.sh
+++ src/components/C++/cpp.sh
@@ -5,11 +5,11 @@ function cpp.run () {
   #    and if it is, run the file with proper
   #    command.
   #  ======================================
-  
+
   local ext=$1 && shift 1
   if [[ ${ext} == "cpp" || ${ext} == "cc" ]]; then
     for file in "$@"; do
-      g++ ${file} -o ${file}.o
+      g++ -std=c++20 ${file} -o ${file}.o
       ./${file}.o
       rm -rvf ${file}.o >/dev/null 2>&1
     done
@@ -21,7 +21,7 @@ function cpp.compile () {
   for file in "$@"; do
     local filename=$(basename -- "${file}")
     filename="${filename%.*}"
-    g++ ${file} -o ${filename}
-  done  
+    g++ -std=c++20 ${file} -o ${filename}
+  done
   return
 }
